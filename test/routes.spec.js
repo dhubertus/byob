6d583a902c09db8120a4a1cb6cf4a8d890a571d9
Dave Hubertus <davehubertus@gmail.com>
2017-07-13 15:15:15 -0600

    added circle and fixed const to var in tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 
 describe("API Routes", () => {
 
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imd1eSIsInBhc3N3b3JkIjoiZmllcmkiLCJpYXQiOjE0OTk5NzU3MTQsImV4cCI6MTUwMTE4NTcxNH0.eyUx3Sf0cKv7I48fyektD3sZdMkyGdLaZ1J30IE7zTY";
+  var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imd1eSIsInBhc3N3b3JkIjoiZmllcmkiLCJpYXQiOjE0OTk5NzU3MTQsImV4cCI6MTUwMTE4NTcxNH0.eyUx3Sf0cKv7I48fyektD3sZdMkyGdLaZ1J30IE7zTY";
 
   before((done) => {
     knex.migrate.latest()
